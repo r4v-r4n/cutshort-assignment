20e546f0cb563dc8ee3dd8efc516fd0b31c1e09a
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-15 01:40:59 +0530

    Update Overrides.ts

--- src/theme/Overrides.ts
+++ src/theme/Overrides.ts
@@ -1,6 +1,6 @@
 // override material-ui styles
 import { createTheme } from '@mui/material/styles';
-
+import colors from 'styles/exports.module.scss';
 export const overrides = createTheme({
 	// overriding material ui button styles
 	components: {
@@ -11,6 +11,17 @@ export const overrides = createTheme({
 				root: {
 					// CSS
 					textTransform: 'none',
+				},
+			},
+		},
+		// updating css for card action area
+		MuiCard: {
+			styleOverrides: {
+				root: {
+					// apply border color on hover
+					'&:hover': {
+						border: `1px solid ${colors.palettePrimaryMain}`,
+					},
 				},
 			},
 		},
